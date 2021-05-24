25e490590185c4d865455a369739f701bd484329
Asebodi <aditraharditya45@gmail.com>
2021-05-24 21:11:00 +0700

    Added firebase config

---
+++ firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAkvKREUK4OkYQ92DWtp_JiZ-EEdrAO0YQ",
+  authDomain: "skripsi-form.firebaseapp.com",
+  projectId: "skripsi-form",
+  storageBucket: "skripsi-form.appspot.com",
+  messagingSenderId: "672272971129",
+  appId: "1:672272971129:web:d0f50a071b282b593aaba8",
+  measurementId: "G-QB0SYRXZ68",
+};
