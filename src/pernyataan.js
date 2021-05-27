f20709df63e745b1f7e3057c5ab5766b37c17ef9
Asebodi <aditraharditya45@gmail.com>
2021-05-27 20:29:16 +0700

    Imported real statements

--- src/pernyataan.js
+++ src/pernyataan.js
@@ -56,6 +56,167 @@ const pernyataan = [
       },
     ],
   },
+
+  {
+    title: "Social Influence",
+    titleInd: "Pengaruh Sosial",
+    id: "SI",
+    color: "bg-green-500",
+
+    items: [
+      {
+        itemId: "SI1",
+        isNegative: true,
+        question:
+          "Saya sudah melihat aplikasi Lapor Sleman digunakan oleh orang lain.",
+      },
+      {
+        itemId: "SI2",
+        isNegative: false,
+        question:
+          "Orang-orang di sekitar saya merekomendasikan aplikasi Lapor Sleman ketika melakukan pengaduan.",
+      },
+      {
+        itemId: "SI3",
+        isNegative: false,
+        question:
+          "Saya tertarik menggunakan aplikasi Lapor Sleman karena mengikuti orang lain.",
+      },
+    ],
+  },
+
+  {
+    title: "Facilitating Conditions",
+    titleInd: "Kondisi Fasilitas",
+    id: "FC",
+    color: "bg-green-500",
+
+    items: [
+      {
+        itemId: "FC1",
+        isNegative: true,
+        question:
+          "Adanya dukungan pemerintah terhadap aplikasi Lapor Sleman membuat saya menggunakan aplikasi ini.",
+      },
+      {
+        itemId: "FC2",
+        isNegative: false,
+        question:
+          "Saya merasa bahwa penggunaan aplikasi Lapor Sleman didukung dengan sumber daya yang memadai.",
+      },
+      {
+        itemId: "FC3",
+        isNegative: false,
+        question:
+          "Saya dapat menggunakan aplikasi Lapor Sleman kapan saja tanpa batasan waktu.",
+      },
+      {
+        itemId: "FC4",
+        isNegative: false,
+        question:
+          "Saya memiliki perangkat yang dibutuhkan untuk menggunakan aplikasi Lapor Sleman.",
+      },
+      {
+        itemId: "FC5",
+        isNegative: false,
+        question:
+          "Saya memiliki pengetahuan yang dibutuhkan untuk menggunakan aplikasi Lapor Sleman.",
+      },
+    ],
+  },
+
+  {
+    title: "Perceived Risk",
+    titleInd: "Risiko yang Dirasakan",
+    id: "PR",
+    color: "bg-green-500",
+
+    items: [
+      {
+        itemId: "PR1",
+        isNegative: true,
+        question:
+          "Saya merasa tidak nyaman secara psikologis ketika menggunakan aplikasi Lapor Sleman.",
+      },
+      {
+        itemId: "PR2",
+        isNegative: false,
+        question:
+          "Saya merasa bahwa terdapat berbagai risiko ketika menggunakan aplikasi Lapor Sleman.",
+      },
+      {
+        itemId: "PR3",
+        isNegative: false,
+        question:
+          "Aplikasi Lapor SLeman memungkinkan data-data saya diambil atau disalahgunakan.",
+      },
+      {
+        itemId: "PR4",
+        isNegative: false,
+        question:
+          "Saya merasa bahwa penggunaan aplikasi Lapor Sleman tidak aman karena alasan privasi atau keamanan.",
+      },
+      {
+        itemId: "PR5",
+        isNegative: false,
+        question:
+          "Saya merasa bahwa aplikasi Lapor Sleman memiliki konsekuensi buruk kedepannya.",
+      },
+    ],
+  },
+
+  {
+    title: "Attitude",
+    titleInd: "???",
+    id: "A",
+    color: "bg-green-500",
+
+    items: [
+      {
+        itemId: "A1",
+        isNegative: true,
+        question: "Menggunakan aplikasi Lapor Sleman merupakan ide yang baik.",
+      },
+      {
+        itemId: "A2",
+        isNegative: false,
+        question: "Saya sangat terbantu dengan adanya aplikasi Lapor Sleman.",
+      },
+      {
+        itemId: "A3",
+        isNegative: false,
+        question:
+          "Saya merasa bahwa aplikasi Lapor Sleman merpakan cara terbaik dalam melakukan pengaduan.",
+      },
+    ],
+  },
+
+  {
+    title: "Behavioral Intention",
+    titleInd: "???",
+    id: "BI",
+    color: "bg-green-500",
+
+    items: [
+      {
+        itemId: "BI1",
+        isNegative: true,
+        question: "Saya setuju dengan diterapkannya aplikasi Lapor Sleman.",
+      },
+      {
+        itemId: "BI2",
+        isNegative: false,
+        question:
+          "Saya menggunakan aplikasi Lapor Sleman dalam jangka waktu yang panjang.",
+      },
+      {
+        itemId: "BI3",
+        isNegative: false,
+        question:
+          "Saya akan merekomendasikan aplikasi Lapor Sleman kepada masyarakat lainnya.",
+      },
+    ],
+  },
 ];
 
 export default pernyataan;
